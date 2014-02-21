71354e79089bb52d6096736f0422ace372161e39
Copot Matei <matei@copot.eu>
2014-02-21 17:41:37 +0100

    changed site

---
+++ head.php
@@ -0,0 +1,8 @@
+<html ng-app='ng-app'>
+	<head>
+		<meta charset='utf-8'>
+		<link rel='stylesheet' type='text/css' href='http://www.copot.eu/t1wc/assets/universal-style.css'/>
+		<script src='/t1wc/assets/libraries/angular.js'></script>
+		<script src='/t1wc/assets/libraries/jquery.js'></script>
+		<script src='/t1wc/assets/universal-scripts.js'></script>
+		<title>
